1cd9b417ca772e7653b4efa4fc137768c5135489
Albinet Bogdan-Florin <33552699+BoggyAF@users.noreply.github.com>
2021-01-08 20:54:44 +0200

    updated about me page and fixed some scaling bugs

---
+++ ia/ia/scripts.js
@@ -0,0 +1,53 @@
+// Functie Nav Hamburger
+
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+const navBar = document.getElementById('nav-bar');
+
+hamburger.addEventListener('click', () => {
+	navUL.classList.toggle('show');
+	navBar.classList.toggle('show');
+})
+
+
+
+
+// Functie Form
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  var form = document.getElementById("contact-form");
+  // var button = document.getElementById("submit");
+  var status = document.getElementById("status");
+  var email = document.getElementById("email");
+
+  // Functii status
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error(response) {
+    status.classList.add("error");
+    status.innerHTML = response;
+  }
+
+  // Form Submission Event
+
+  form.addEventListener("submit", function (event) {
+	event.preventDefault()
+    Post(email, success, error);
+  });
+});
+
+// Helper Func pt "request"
+
+function Post(data, success, error) {
+    if (data.value.includes("@")&&data.value.includes(".")) {
+      success();
+    } else {
+      error("Please make sure to enter a valid email adress.");
+    };
+}
